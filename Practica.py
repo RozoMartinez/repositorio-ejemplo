ee0720928a9405c6d4337371f4ce7b6392c31138
RozoMartinez <lilirozomartinez@gmail.com>
2022-08-20 11:34:34 -0500

    inicializar página web con un index

---
+++ Practica.py
@@ -0,0 +1,8 @@
+a=input('ingrese el valor del articulo')
+b=input('ingrese el valor del iva')
+c=int(a)
+d=int(b)
+iva=c*d/100
+precio_T=c+iva
+print('el valor del articulo es:',c,'el valor del iva es:',iva,'el precio total es:',precio_T)
+
